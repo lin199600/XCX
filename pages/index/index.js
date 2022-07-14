cfc3d3ade823bd3013c445f831945672d90621da
lin <281403574@qq.com>
2022-07-14 23:45:43 +0800

    新增排行榜

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,18 +1,47 @@
-// pages/index/index.js
+import request from '../../utils/request'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    bannerList: [],  //轮播图数据
+    recommendList: [], //推荐歌曲数据
+    topList: []  //排行榜数据
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad(options) {
+  async onLoad(options) {
+    // 获取轮播图数据
+    let banner = await request('/banner',{type: 1})
+    if(banner.code === 200){
+      this.setData({
+        bannerList : banner.banners
+      })
+    }
 
+    // 推荐歌曲数据
+    let recommend = await request('/personalized', {limit: 10})
+    if(recommend.code === 200){
+      this.setData({
+        recommendList: recommend.result
+      })
+    }
+
+    // 排行榜数据
+    let index = 0
+    let resultArr = []
+    while(index<5){
+      let top = await request('/top/list', {idx: index++})
+      let topItem = {name: top.playlist.name, tracks: top.playlist.tracks.slice(0,3)}
+      resultArr.push(topItem)
+      this.setData({
+        topList: resultArr
+      })
+    }
   },
 
   /**
